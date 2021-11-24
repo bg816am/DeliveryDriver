35fa796a735c0273af581452162848f060e24439
Brian G <brian.gould@gmail.com>
2021-11-24 09:36:30 -0500

    Cleaning code/Map
    Moving Map tiles and cleaning up code

--- Assets/Scripts/FollowCamera.cs
+++ Assets/Scripts/FollowCamera.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class FollowCamera : MonoBehaviour
 {
+    Vector3 offset = new Vector3(0, 0, -10);
     [SerializeField] GameObject ObjectToFollow;
     // Camera position = Car position
     void LateUpdate()
     {
-       transform.position = ObjectToFollow.transform.position + new Vector3(0, 0, -10);
+       transform.position = ObjectToFollow.transform.position + offset;
     }
 }
