95320934701ecd88e8933eaf88dc69577c2310c4
Brian G <brian.gould@gmail.com>
2021-11-27 23:10:30 -0500

    Updated Camera
    Added Cinemachine to game to remove follow script.

--- Assets/Scripts/FollowCamera.cs
+++ Assets/Scripts/FollowCamera.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// Due to adding of Cinemachine to camera, this script is only here as
+// a reference and is not added in.
 public class FollowCamera : MonoBehaviour
 {
     Vector3 offset = new Vector3(0, 0, -10);
